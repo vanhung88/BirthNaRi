f05e35e2aec3508a83ea201bef430b61d18ac1ed
unknown <88levanhung@gmail.com>
2021-05-26 23:35:47 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+Dao = () => {
+    document.querySelector('.flip-box-inner').style.transform= "rotateX(0deg)";
+};
+Dao1 = () => {
+    document.querySelector('.flip-box-inner1').style.transform= "rotateX(0deg)";
+};
+Dao2 = () => {
+    document.querySelector('.flip-box-inner2').style.transform= "rotateX(0deg)";
+};
+Dao3 = () => {
+    document.querySelector('.flip-box-inner3').style.transform= "rotateX(0deg)";
+};
+Dao4 = () => {
+    document.querySelector('.flip-box-inner4').style.transform= "rotateX(0deg)";
+};
+Dao5 = () => {
+    document.querySelector('.flip-box-inner5').style.transform= "rotateX(0deg)";
+};
+Dao6 = () => {
+    document.querySelector('.flip-box-inner6').style.transform= "rotateX(0deg)";
+};
+Dao7 = () => {
+    document.querySelector('.flip-box-inner7').style.transform= "rotateX(0deg)";
+};
+Dao8 = () => {
+    document.querySelector('.flip-box-inner8').style.transform= "rotateX(0deg)";
+};
+Dao9 = () => {
+    document.querySelector('.flip-box-inner9').style.transform= "rotateX(0deg)";
+};
+Dao10= () => {
+    document.querySelector('.flip-box-inner10').style.transform= "rotateX(0deg)";
+};
+Dao11 = () => {
+    document.querySelector('.flip-box-inner11').style.transform= "rotateX(0deg)";
+};
+
+// onmouseout=()=>{
+//     document.querySelector('.flip-box-inner').style.transform= "rotateX(180deg)";
+
+// }
